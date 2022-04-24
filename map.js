34b103a58cc3528560abf23ff4140b9a8bff677f
dgndds <57489727+dgndds@users.noreply.github.com>
2022-04-24 17:35:38 +0300

    Add files via upload

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-let map, locWindow;
+let map, locWindow, deviceWindow, poleWindow;
 var marker
 
 
@@ -7,7 +7,10 @@ function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 6,
   });
+
   locWindow = new google.maps.InfoWindow();
+  deviceWindow = new google.maps.InfoWindow();
+  poleWindow = new google.maps.InfoWindow();
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -19,12 +22,22 @@ function initMap() {
         
         const currentLoc = pos;
         const northPole = {lat: 72.68, lng: 80.65};
-        // The markers for The Dakota and The Frick Collection
+
+        deviceWindow.setPosition(currentLoc);
+        deviceWindow.setContent("Your Location");
+        deviceWindow.open(map);
+
+        poleWindow.setPosition(northPole);
+        poleWindow.setContent("North Pole");
+        poleWindow.open(map);
+
+
         var mk1 = new google.maps.Marker({position: currentLoc, map: map});
         var mk2 = new google.maps.Marker({position: northPole, map: map});
         var line = new google.maps.Polyline({path: [currentLoc, northPole], map: map});
         var distance = haversine_distance(mk1, mk2);
         document.getElementById('distancePoints').innerHTML = "Distance between markers: " + distance.toFixed(2) + " km.";
+        map.setCenter(pos)
     })
   }
 
@@ -38,7 +51,6 @@ function initMap() {
     var lat = parseFloat(document.getElementById('locLat').value);
     var lng = parseFloat(document.getElementById('locLng').value);
     var pos = new google.maps.LatLng(lat, lng);
-    console.log(pos)
     
     locWindow.setPosition(pos);
     locWindow.setContent("Latitude: " + pos.lat() + " Longitude: " + pos.lng());
@@ -51,11 +63,11 @@ function initMap() {
 }
 
 function haversine_distance(mk1, mk2) {
-  var R = 6371.0710; // Radius of the Earth in miles
-  var rlat1 = mk1.position.lat() * (Math.PI/180); // Convert degrees to radians
-  var rlat2 = mk2.position.lat() * (Math.PI/180); // Convert degrees to radians
-  var difflat = rlat2-rlat1; // Radian difference (latitudes)
-  var difflon = (mk2.position.lng()-mk1.position.lng()) * (Math.PI/180); // Radian difference (longitudes)
+  var R = 6371.0710;
+  var rlat1 = mk1.position.lat() * (Math.PI/180);
+  var rlat2 = mk2.position.lat() * (Math.PI/180);
+  var difflat = rlat2-rlat1;
+  var difflon = (mk2.position.lng()-mk1.position.lng()) * (Math.PI/180);
 
   var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
   return d;
@@ -68,13 +80,13 @@ var J2000 = 2451545;
 function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
 function toDays(date)   { return toJulian(date) - J2000; }
 
-function moonDistance(d) { // geocentric ecliptic coordinates of the moon
+function moonDistance(d) {
   var PI   = Math.PI;
   var cos  = Math.cos;
   var rad  = PI / 180;
-  
-  var M = rad * (134.963 + 13.064993 * d), // mean anomaly
-  dt = 385001 - 20905 * cos(M);  // distance to the moon in km
+
+  var M = rad * (134.963 + 13.064993 * d),
+  dt = 385001 - 20905 * cos(M);
 
   return dt
 }
