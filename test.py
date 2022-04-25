dca385c744d06f08ce70f9c057460eae0a58d456
dgndds <57489727+dgndds@users.noreply.github.com>
2022-04-25 17:31:12 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import time
 import sys
 import logging
 from selenium.webdriver.remote.remote_connection import LOGGER
+from random import randint
 
 LOGGER.setLevel(logging.WARNING)
 
@@ -31,6 +32,7 @@ if len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Latitude: 
     print("Locator Verification Test 1: Successful")
 else:
     print("Locator Verification Test 1: Failed")
+    sys.exit()
 
 time.sleep(2)
 
@@ -46,6 +48,7 @@ if len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Latitude: 
     print("Locator Verification Test 2: Successful")
 else:
     print("Locator Verification Test 2: Failed")
+    sys.exit()
 
 time.sleep(2)
 
@@ -55,6 +58,7 @@ locateButton.click()
 
 if(driver.find_element(By.ID,"lngError").is_displayed() or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Latitude: 86 Longitude: 12')]")) > 0 or not driver.find_element(By.ID,"latError").is_displayed()):
     print("Latitude Error Test: Failed")
+    sys.exit()
 else:
     print("Latitude Error Test: Successful")
 
@@ -70,6 +74,7 @@ time.sleep(2)
 
 if(driver.find_element(By.ID,"latError").is_displayed() or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Latitude: 41 Longitude: 181')]")) > 0 or not driver.find_element(By.ID,"lngError").is_displayed()):
     print("Longitude Error Test: Failed")
+    sys.exit()
 else:
     print("Longitude Error Test: Successful")
 
@@ -85,6 +90,7 @@ time.sleep(2)
 
 if(not driver.find_element(By.ID,"latError").is_displayed() or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Latitude: 86 Longitude: 181')]")) > 0 or not driver.find_element(By.ID,"lngError").is_displayed()):
     print("Latitude & Longitude Error Test: Failed")
+    sys.exit()
 else:
     print("Latitude & Longitude Error Test: Successful")
 
@@ -98,6 +104,7 @@ locateButton.click()
 
 if(latitudeInput.get_attribute("value") == "Doğancan" or longitudeInput.get_attribute("value") == "Ertuğrul"):
     print("Invalid Character Test: Failed")
+    sys.exit()
 else:
     print("Invalid Character Test: Successful")
 
@@ -119,6 +126,7 @@ locateButton.click()
 
 if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Your Location')]")) <= 0 or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'North Pole')]")) <= 0):
     print("Current Location Label Conflict and North Pole Label Conflict Test 1: Failed")
+    sys.exit()
 else:
     print("Current Location Label Conflict and North Pole Label Conflict Test 1: Successful")
 
@@ -132,6 +140,7 @@ locateButton.click()
 
 if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Your Location')]")) > 0 or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'North Pole')]")) <= 0):
     print("Current Location Label Conflict and North Pole Label Conflict Test 2: Failed")
+    sys.exit()
 else:
     print("Current Location Label Conflict and North Pole Label Conflict Test 2: Successful")
 
@@ -145,6 +154,7 @@ locateButton.click()
 
 if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Your Location')]")) <= 0 or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'North Pole')]")) <= 0):
     print("Current Location Label Conflict and North Pole Label Conflict Test 3: Failed")
+    sys.exit()
 else:
     print("Current Location Label Conflict and North Pole Label Conflict Test 3: Successful")
 
@@ -158,15 +168,88 @@ locateButton.click()
 
 if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'North Pole')]")) > 0 or len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Your Location')]")) <= 0):
     print("Current Location Label Conflict and North Pole Label Conflict Test 4: Failed")
+    sys.exit()
 else:
     print("Current Location Label Conflict and North Pole Label Conflict Test 4: Successful")
 
+#======================== Test Case 3 ========================
+print("====== Test Case 3 ======")
 
+time.sleep(2)
+if(len(driver.find_elements(By.ID,"distancePoints")) <= 0):
+    print("Distance Between Two Points Label Test: Failed")
+    sys.exit()
+else:
+    print("Distance Between Two Points Label Test: Successful")
 
+    distancePoints = driver.find_element(By.ID,"distancePoints")
+    distancePointStr = distancePoints.get_attribute("innerHTML")
+    distancePointValue = float(distancePointStr[len("Distance between markers: "):-4])
 
+    if(distancePointValue >= 4370.05 or distancePointValue <= 4572.28):
+        print("Distance Between Two Points Value Test: Successful")
+    else:
+        print("Distance Between Two Points Value Test: Failed")
+        sys.exit()
 
-time.sleep(5)
+#======================== Test Case 4 ========================
+print("====== Test Case 4 ======")
 
+time.sleep(2)
+if(len(driver.find_elements(By.ID,"distanceMoon")) <= 0):
+    print("Distance Between Point and Moon Label Test: Failed")
+    sys.exit()
+else:
+    print("Distance Between Point and Moon Label Test: Successful")
+
+    distanceMoon = driver.find_element(By.ID,"distanceMoon")
+    distanceMoonStr = distanceMoon.get_attribute("innerHTML")
+    distanceMoonValue = float(distanceMoonStr[len("Distance between Moon and Point: "):-4])
+
+    if(distanceMoonValue >= 363105.021 or distanceMoonValue <= 405696.31):
+        print("Distance to Moon Value Test: Successful")
+    else:
+        print("Distance to Moon Value Test: Failed")
+        sys.exit()
+
+#======================== Test Case 5 ========================
+print("====== Test Case 5 ======")
+prevLats = []
+prevLngs = []
+
+for i in range(0,10):
+    lat = randint(-85,85)
+    lng = randint(-180,180)
+
+    time.sleep(0.5)
+
+    latitudeInput.clear()
+    latitudeInput.send_keys(str(lat))
+    longitudeInput.clear()
+    longitudeInput.send_keys(str(lng))
+    locateButton.click()
+
+    prevLats.append(lat)
+    prevLngs.append(lng)
+
+for i in range(0,9):
+    value = "Latitude: " + str(prevLats[i]) + " Longitude: " + str(prevLngs[i])
+
+    if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), '%s')]"%value)) > 0):
+        print("Rapid Valid Input Test: Failed")
+        print("At Lat: %s Lng: %s"%(str(prevLats[i]),str(prevLngs[i])))
+        sys.exit()
+
+value = "Latitude: " + str(prevLats[9]) + " Longitude: " + str(prevLngs[9])    
+if(len(driver.find_elements(by=By.XPATH, value="//*[contains(text(), '%s')]"%value)) <= 0):
+    print("Rapid Valid Input Test: Failed")
+    print("At Lat: %s Lng: %s"%(str(prevLats[9]),str(prevLngs[9])))
+    sys.exit()
+else:
+    print("Rapid Valid Input Test: Successful")
+
+print("All Tests Passed Successfully!")
+time.sleep(2)
 driver.close()
 
 
